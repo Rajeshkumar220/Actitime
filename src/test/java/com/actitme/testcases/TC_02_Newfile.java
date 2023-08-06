8eb0b6229661d37c0702ca21e33e7a3c0bd3162a
Rajeshkumar220 <rajeshbicky123@gmail.com>
2023-08-07 01:09:19 +0530

    new file TC_02 added

---
+++ src/test/java/com/actitme/testcases/TC_02_Newfile.java
@@ -0,0 +1,13 @@
+package com.actitme.testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_02_Newfile
+{
+@Test
+public void demo()
+{
+Reporter.log("demo file");	
+}
+}
